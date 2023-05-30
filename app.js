13d27e99600238ff7b3d9481aed44f90a2f55661
Camfabjeh <camille.fabry@yahoo.com>
2023-05-30 11:21:35 +0200

    création userHandlers

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const Joi = require("joi");
+const {validateMovie, validateUser} = require("./validators.js");
+
+require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 
-const port = 5000;
+const port = process.env.APP_PORT ?? 5000;
 
 const welcome = (req, res) => {
   res.send("Welcome to my favourite movie list");
@@ -14,6 +19,18 @@ const movieHandlers = require("./movieHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.get("/api/users", movieHandlers.getUsers);
+app.get("/api/users/:id", movieHandlers.getUserById);
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.post("/api/users", validateUser, movieHandlers.postUser);
+
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
+app.put("/api/users/:id", validateUser, movieHandlers.updateUser);
+
+app.delete("/api/movies/:id", deleteMovie, movieHandlers.deleteMovie);
+app.delete("/api.users/:id", deleteUser, movieHandlers.deleteUser);
 
 app.listen(port, (err) => {
   if (err) {
